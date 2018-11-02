618e5687d8c50d4d039c7198cd41ae5931ec930a
Erik Timmers <erik.timmers@dimenco.eu>
2018-11-02 09:38:01 +0100

    Log backend times of unsynced cameras

--- main.cpp
+++ main.cpp
@@ -3,20 +3,47 @@
 #include <chrono>
 #include <thread>
 
+typedef struct {
+    std::string serial;
+    rs2::pipeline pipeline;
+} Cam;
+
+Cam start_device(rs2::device device) {
+    std::string serial_number(device.get_info(RS2_CAMERA_INFO_SERIAL_NUMBER));
+    std::cout << "Found camera with serial " << serial_number << std::endl;
+
+    rs2::config config;
+    config.enable_device(serial_number);
+    config.enable_stream(RS2_STREAM_INFRARED, 1, 848, 480, RS2_FORMAT_Y8, 90);
+
+    rs2::pipeline pipeline;
+    rs2::pipeline_profile profile = pipeline.start(config);
+    profile.get_device().first<rs2::depth_sensor>().set_option(RS2_OPTION_INTER_CAM_SYNC_MODE, 0);
+
+    Cam cam = { serial_number, pipeline };
+    return cam;
+}
+
 int main(int argc, char * argv[]) try
 {
     std::cout << "Start!" << std::endl;
 
     rs2::log_to_console(RS2_LOG_SEVERITY_ERROR);
-    rs2::colorizer color_map;
-    rs2::pipeline pipe;
-    pipe.start();
+    rs2::context ctx;
+    std::vector<Cam> cams;
 
-    while(true) {
-        rs2::frameset data = pipe.wait_for_frames();
-	rs2_metadata_type backend = data.get_frame_metadata(RS2_FRAME_METADATA_BACKEND_TIMESTAMP);
-	rs2_metadata_type counter = data.get_frame_metadata(RS2_FRAME_METADATA_FRAME_COUNTER);
-	std::cout << counter << " " << backend << std::endl;
+    for (auto&& device : ctx.query_devices()) {
+        cams.push_back(start_device(device));
+    }
+
+    while (true) {
+        for (auto it = cams.begin(); it != cams.end(); ++it) {
+            auto cam = *it;
+            auto data = cam.pipeline.wait_for_frames();
+            auto backend = data.get_frame_metadata(RS2_FRAME_METADATA_BACKEND_TIMESTAMP);
+            auto counter = data.get_frame_metadata(RS2_FRAME_METADATA_FRAME_COUNTER);
+            std::cout << cam.serial << " " << counter << " " << backend << std::endl;
+        }
     }
 
     return 0;
@@ -28,6 +55,6 @@ catch (const rs2::error & e)
 }
 catch (const std::exception& e)
 {
-    std::cerr << e.what() << std::endl;
+    std::cerr << "Exception " << e.what() << std::endl;
     return 1;
 }
